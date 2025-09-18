ad160ba8b455c322d97276b658d2104a9b9f20da
akhileshyrofficial-max <akhileshyrofficial@gmail.com>
2025-09-18 13:08:47 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,11 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/roadguard-ai/',  // 👈 important for GitHub Pages
     plugins: [react()],
-    base: '/roadguard-ai/', // 👈 IMPORTANT: must match your GitHub repo name
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
